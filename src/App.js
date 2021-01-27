e8075f34bd493ecf26dd0c176d950e372565d55e
Alex Kayser <akctba@gmail.com>
2021-01-27 10:10:47 -0500

    warnings

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { Suspense, useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
--- src/components/result/Result.js
+++ src/components/result/Result.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useContext, useLayoutEffect } from "react";
 import { Box, Text, Button } from 'grommet';
 import { ShareOption } from 'grommet-icons';
