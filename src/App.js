ff1bb7bd2c7d855c95da1f5b650af33a04eb70fc
Alex Kayser <akctba@gmail.com>
2021-01-27 00:15:40 -0500

    Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Box, Text, Grommet, Heading} from 'grommet';
+import {Box, Text, Grommet, Heading, Header, Anchor, Nav} from 'grommet';
+//import { Projects } from 'grommet-icons';
 import OptionList from './components/options/OptionList';
 
 import { OptionProvider } from './context/Context';
@@ -17,9 +18,17 @@ const theme = {
 
 function App() {
     return (
-        <Grommet full
-            theme={theme}>
-            <header className="App-header">
+        <Grommet full theme={theme}>
+            {/* <Header pad="small" height="xxsmall" background="dark-1">
+              <Anchor
+                  href="https://tools.grommet.io/"
+                  icon={<Projects />}
+                  label="Thinking about hiring a project, job or service?"
+                />
+                <Nav direction="row">
+                  <Anchor label="Profile" href="#" />
+                </Nav>
+            </Header> */}
 
                 <Box fill={true}
                     background="dark-1"
@@ -44,7 +53,6 @@ function App() {
                     {/* ADDS */} 
 
                   </Box>
-            </header>
         </Grommet>
     );
 }
